7ca7dc90e893982778e5c1baa726533b303e1454
Aniket Sen <136061264+AniketSen03@users.noreply.github.com>
2026-07-15 13:51:20 +0530

    Time: 1 ms (83.11%), Space: 42.6 MB (57.16%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,15 @@
+//class Solution {
+  //  public int gcdOfOddEvenSums(int n) {
+     // return gcd(n * n, n * (n + 1));
+   // }
+    
+   // private int gcd(int a, int b) {
+   //     return b == 0 ? a : gcd(b, a % b);
+//    }
+//}
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
